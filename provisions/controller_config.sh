dbcf002f593af82720403cc92b5fd3a0a6221696
RajsJohal <rajsjohal97@hotmail.com>
2022-01-04 17:42:46 +0000

    added images

--- provisions/controller_config.sh
+++ provisions/controller_config.sh
@@ -17,8 +17,8 @@ sudo apt-get install ansible -y
 
 
 # add fingerprint authentication and copy host folder into controller VM
-ssh-keyscan -H 192.168.33.10 >> ~/.ssh/known_hosts
+ssh-keyscan -H 192.168.33.10 >> /home/vagrant/.ssh/known_hosts
 
-ssh-keyscan -H 192.168.33.11 >> ~/.ssh/known_hosts
+ssh-keyscan -H 192.168.33.11 >> /home/vagrant/.ssh/known_hosts
 
 sudo cp /home/vagrant/controller/hosts /etc/ansible/hosts
