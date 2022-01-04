e81f7d18342568d9163162f0dec3ecb876cf97d4
RajsJohal <rajsjohal97@hotmail.com>
2022-01-04 16:25:54 +0000

    added hosts file

--- provisions/controller_config.sh
+++ provisions/controller_config.sh
@@ -14,3 +14,9 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update -y
 
 sudo apt-get install ansible -y
+
+ssh-keyscan -H 192.168.33.10 >> ~/.ssh/known_hosts
+
+ssh-keyscan -H 192.168.33.11 >> ~/.ssh/known_hosts
+
+sudo cp ./provisions/hosts /etc/ansible/hosts
